2494667523dc4e6961376c277a92026cfc9bfe7c
YaCpotato <yasshisshy@gmail.com>
2020-04-16 21:00:59 +0900

    nextトークンを利用したアクセス完成

--- get_x_ninja_tweet.py
+++ get_x_ninja_tweet.py
@@ -3,6 +3,39 @@ import json, config
 from requests_oauthlib import OAuth1Session
 import csv
 
+def searchWordsRecurrent(req,from_date, to_date):
+    url = "https://api.twitter.com/1.1/tweets/search/fullarchive/MyPortfolio.json"
+    keyword = "Ma_gician"
+    print('----------------------------------------------------')
+    params = {'query' : keyword, 'maxResults' : 100,'fromDate':from_date,'toDate':to_date, 'next':req['next']}
+    result = twitter.get(url, params = params)
+    header = ['id','User Name','User ID','Follows','Followers','User Location','content','time']
+    search_timeline = {}
+    if result.status_code == 200:
+         with open('data/{keyword}from{from_date}_to{to_date}.csv'.format(keyword = keyword, from_date = from_date, to_date = to_date), 'w') as f:
+             search_timeline = json.loads(result.text)
+             writer = csv.writer(f)
+             writer.writerow(header)
+             for tweet in search_timeline['results']:
+                 tmp = []
+                 tmp.append(tweet['id'])
+                 tmp.append(tweet['user']['name'])
+                 tmp.append(tweet['user']['screen_name'])
+                 tmp.append(tweet['user']['friends_count'])
+                 tmp.append(tweet['user']['followers_count'])
+                 tmp.append(tweet['user']['location'])
+                 tmp.append(tweet['text'])
+                 tmp.append(tweet['created_at'])
+                 writer.writerow(tmp)
+                 tmp = []
+             print(len(search_timeline['results']))
+    else:
+        print("ERROR: %d" % req.status_code)
+    if 'next' in search_timeline:
+        searchWordsRecurrent(search_timeline,from_date, to_date)
+
+    return;
+
 
 def searchWords(from_date, to_date):
     url = "https://api.twitter.com/1.1/tweets/search/fullarchive/MyPortfolio.json"
@@ -11,6 +44,7 @@ def searchWords(from_date, to_date):
     params = {'query' : keyword, 'maxResults' : 100,'fromDate':from_date,'toDate':to_date}
     req = twitter.get(url, params = params)
     header = ['id','User Name','User ID','Follows','Followers','User Location','content','time']
+    search_timeline = {}
     if req.status_code == 200:
         with open('data/{keyword}from{from_date}_to{to_date}.csv'.format(keyword = keyword, from_date = from_date, to_date = to_date), 'w') as f:
             search_timeline = json.loads(req.text)
@@ -29,9 +63,11 @@ def searchWords(from_date, to_date):
                 writer.writerow(tmp)
                 tmp = []
             print(len(search_timeline['results']))
-            return;
     else:
         print("ERROR: %d" % req.status_code)
+    if 'next' in search_timeline:
+        searchWordsRecurrent(search_timeline,"201906010000", "201907010000")
+
     return;
 
 if __name__ == '__main__':
@@ -40,4 +76,4 @@ if __name__ == '__main__':
     AT = config.ACCESS_TOKEN
     ATS = config.ACCESS_TOKEN_SECRET
     twitter = OAuth1Session(CK, CS, AT, ATS)
-    searchWords("201908010000", "201908100000")
+    searchWords("201906010000", "201907010000")
